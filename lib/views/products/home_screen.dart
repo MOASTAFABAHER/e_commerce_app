c56cba84dbe19c6528aad7ffd4d8ce1218c8c487
user <mostafabaher403@gmail.com>
2023-02-18 21:58:56 +0200

    after add favorite

--- lib/views/products/home_screen.dart
+++ lib/views/products/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/bloc/Payment_cubit/cubit/k_y_c_cubit.dart';
 import 'package:e_commerce_app/components/products/List_of_best_seller.dart';
 import 'package:e_commerce_app/components/products/all_item_grid_view.dart';
 
@@ -9,6 +10,7 @@ import 'package:e_commerce_app/utils/app_navigator.dart';
 import 'package:e_commerce_app/views/products/cart_screen.dart';
 import 'package:e_commerce_app/views/settings/List_of_categories_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../components/categories/get_all_categories_list.dart';
@@ -21,74 +23,82 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      drawer: ListDrawer(),
-      backgroundColor: AppColors.kWhiteColor,
-      appBar: AppBar(
-        backgroundColor: AppColors.kWhiteColor,
-        elevation: 0,
-        leading: IconButton(
-          onPressed: () {
-            scaffoldKey.currentState?.openDrawer();
-          },
-          icon: Icon(
-            Icons.menu,
-            size: 25.sp,
-            color: AppColors.kGreyColor,
-          ),
-        ),
-        title: SizedBox(
-          height: 75.h,
-          child: CustomSearchField(
-            lable: 'Search',
-          ),
-        ),
-        actions: [
-          IconButton(
-              onPressed: () {
-                AppNavigator.appNavigator(context, CartScreen(),
-                    isFinished: true);
-              },
-              icon: const Icon(
-                Icons.shopping_cart,
-                color: AppColors.kGreyColor,
-              )),
-        ],
-      ),
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Column(
-          children: [
-            const CustomRowSlider(),
-            SizedBox(
-              height: 10.h,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: Column(
-                children: [
-                  TopicText(
-                    text: 'Categories',
-                    function: () {
-                      AppNavigator.appNavigator(
-                          context, const ListOfCategoriesScreen(),
+    return BlocProvider(
+      create: (context) => KYCCubit()..getAuthToken(),
+      child: BlocConsumer<KYCCubit, KYCState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return Scaffold(
+            key: scaffoldKey,
+            drawer: ListDrawer(),
+            backgroundColor: AppColors.kWhiteColor,
+            appBar: AppBar(
+              backgroundColor: AppColors.kWhiteColor,
+              elevation: 0,
+              leading: IconButton(
+                onPressed: () {
+                  scaffoldKey.currentState?.openDrawer();
+                },
+                icon: Icon(
+                  Icons.menu,
+                  size: 25.sp,
+                  color: AppColors.kGreyColor,
+                ),
+              ),
+              title: SizedBox(
+                height: 75.h,
+                child: CustomSearchField(
+                  lable: 'Search',
+                ),
+              ),
+              actions: [
+                IconButton(
+                    onPressed: () {
+                      AppNavigator.appNavigator(context, CartScreen(),
                           isFinished: true);
                     },
-                  ),
-                  const GetAllCategoriesList(),
-                  TopicText(text: 'Best Seller'),
-                  const ListOfBestSeller(),
+                    icon: const Icon(
+                      Icons.shopping_cart,
+                      color: AppColors.kGreyColor,
+                    )),
+              ],
+            ),
+            body: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  const CustomRowSlider(),
                   SizedBox(
                     height: 10.h,
                   ),
-                  TopicText(text: 'All Item'),
-                  const AllItemGridView()
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Column(
+                      children: [
+                        TopicText(
+                          text: 'Categories',
+                          function: () {
+                            AppNavigator.appNavigator(
+                                context, const ListOfCategoriesScreen(),
+                                isFinished: true);
+                          },
+                        ),
+                        const GetAllCategoriesList(),
+                        TopicText(text: 'Best Seller'),
+                        const ListOfBestSeller(),
+                        SizedBox(
+                          height: 10.h,
+                        ),
+                        TopicText(text: 'All Item'),
+                        const AllItemGridView()
+                      ],
+                    ),
+                  )
                 ],
               ),
-            )
-          ],
-        ),
+            ),
+          );
+        },
       ),
     );
   }
